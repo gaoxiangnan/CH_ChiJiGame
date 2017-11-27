28da9b1121da31ea9c4c2c65bd30795dc425aede
xngao <gxn@zhongying365.com>
2017-11-27 17:19:53 +0800

    model
    Model

---
+++ CH_ChiJiGame/Model/UserModel.h
@@ -0,0 +1,13 @@
+//
+//  UserModel.h
+//  CH_ChiJiGame
+//
+//  Created by 高向楠 on 2017/11/27.
+//  Copyright © 2017年 高向楠. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserModel : NSObject
+
+@end
---
+++ CH_ChiJiGame/Model/UserModel.m
@@ -0,0 +1,13 @@
+//
+//  UserModel.m
+//  CH_ChiJiGame
+//
+//  Created by 高向楠 on 2017/11/27.
+//  Copyright © 2017年 高向楠. All rights reserved.
+//
+
+#import "UserModel.h"
+
+@implementation UserModel
+
+@end
