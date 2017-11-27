36be227e0a77adc6e5ea5c38d96d6525726cd8fb
yongjian <5859497@qq.com>
2017-11-27 16:47:48 +0800

    url
    url

--- CH_ChiJiGame/ViewController.m
+++ CH_ChiJiGame/ViewController.m
@@ -184,16 +184,20 @@
 -(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
 {
     
-    
-    if (range.length + range.location > textField.text.length) {
-        return NO;
+    if (textField == _IDtext) {
+        if (range.length + range.location > textField.text.length) {
+            return NO;
+        }
+        NSUInteger length = _IDtext.text.length + string.length - range.length;
+        
+        
+        
+        return length <= 11&&[self validateNumber:string];
+    }else{
+        
     }
-    NSUInteger length = _IDtext.text.length + string.length - range.length;
     
-    
-    
-    return length <= 11&&[self validateNumber:string];
-    
+    return YES;
     
 }
 
@@ -245,40 +249,60 @@
 -(void)icodeBtnAciton
 {
     NSLog(@"获取验证码");
-    __block NSInteger time = 59; //倒计时时间
     
-    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-    dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
-    
-    dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0); //每秒执行
-    
-    dispatch_source_set_event_handler(_timer, ^{
-        
-        if(time <= 0){ //倒计时结束，关闭
+    [CH_NetWorkManager getWithURLString:@"sendPhoneMessage" parameters:@{@"phone":_IDtext.text} success:^(NSDictionary *data) {
+        NSLog(@"%@",data);
+        NSLog(@"%@",[data objectForKey:@"message"]);
+        if ([[data objectForKey:@"code"] isEqualToString:@"200"]) {
             
-            dispatch_source_cancel(_timer);
-            dispatch_async(dispatch_get_main_queue(), ^{
+            __block NSInteger time = 59; //倒计时时间
+            
+            dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+            dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+            
+            dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0); //每秒执行
+            
+            dispatch_source_set_event_handler(_timer, ^{
                 
-                //设置按钮的样式
-                [self.icodeBtn setTitle:@"重新发送" forState:UIControlStateNormal];
-                
-                self.icodeBtn.userInteractionEnabled = YES;
+                if(time <= 0){ //倒计时结束，关闭
+                    
+                    dispatch_source_cancel(_timer);
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        
+                        //设置按钮的样式
+                        [self.icodeBtn setTitle:@"重新发送" forState:UIControlStateNormal];
+                        
+                        self.icodeBtn.userInteractionEnabled = YES;
+                    });
+                    
+                }else{
+                    
+                    int seconds = time % 60;
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        
+                        //设置按钮显示读秒效果
+                        [self.icodeBtn setTitle:[NSString stringWithFormat:@"重新发送(%.2d)", seconds] forState:UIControlStateNormal];
+                        
+                        self.icodeBtn.userInteractionEnabled = NO;
+                    });
+                    time--;
+                }
             });
+            dispatch_resume(_timer);
             
         }else{
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"温馨提示" message:[data objectForKey:@"message"] preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction *ac = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction *  action) {
+            }];
             
-            int seconds = time % 60;
-            dispatch_async(dispatch_get_main_queue(), ^{
-                
-                //设置按钮显示读秒效果
-                [self.icodeBtn setTitle:[NSString stringWithFormat:@"重新发送(%.2d)", seconds] forState:UIControlStateNormal];
-                
-                self.icodeBtn.userInteractionEnabled = NO;
-            });
-            time--;
+            [alert addAction:ac];
+            [self presentViewController:alert animated:YES completion:nil];
         }
-    });
-    dispatch_resume(_timer);
+    } failure:^(NSError *error) {
+    
+    }];
+    
+  
 }
 
 #pragma login
@@ -312,10 +336,23 @@
     }else
     {
         
-        CH_TeamCreatViewController *chVC = [[CH_TeamCreatViewController alloc]init];
-        [self.navigationController pushViewController:chVC animated:YES];
-        
-        NSLog(@"登录成功");
+        [CH_NetWorkManager getWithURLString:@"checkCode" parameters:@{@"token":@"8d1dd9db7d026f38ece40cea86abe92b",@"code":_PassWordtext.text} success:^(NSDictionary *data) {
+            if ([[data objectForKey:@"code"]isEqualToString:@"200"]) {
+                CH_TeamCreatViewController *chVC = [[CH_TeamCreatViewController alloc]init];
+                [self.navigationController pushViewController:chVC animated:YES];
+                NSLog(@"%@",[data objectForKey:@"message"]);
+                NSLog(@"%@",data);
+            }else
+            {
+                UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"温馨提示" message:[data objectForKey:@"message"] preferredStyle:UIAlertControllerStyleAlert];
+                UIAlertAction *action = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                }];
+                [alert addAction:action];
+                [self presentViewController:alert animated:YES completion:nil];
+            }
+        } failure:^(NSError *error) {
+            
+        }];
     }
     
     
