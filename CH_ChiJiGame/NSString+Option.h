386b26b37dab0ed00dfd5904e71df081969536fa
xngao <gxn@zhongying365.com>
2017-11-27 16:53:58 +0800

    123321
    123

---
+++ CH_ChiJiGame/NSString+Option.h
@@ -0,0 +1,16 @@
+//
+//  NSString+Option.h
+//  CH_ChiJiGame
+//
+//  Created by 高向楠 on 2017/11/27.
+//  Copyright © 2017年 高向楠. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import<CommonCrypto/CommonDigest.h>
+
+@interface NSString (Option)
+
++ (NSString *) md5:(NSString *) input
+
+@end
---
+++ CH_ChiJiGame/NSString+Option.m
@@ -0,0 +1,24 @@
+//
+//  NSString+Option.m
+//  CH_ChiJiGame
+//
+//  Created by 高向楠 on 2017/11/27.
+//  Copyright © 2017年 高向楠. All rights reserved.
+//
+
+#import "NSString+Option.h"
+
+@implementation NSString (Option)
++ (NSString *) md5:(NSString *) input {
+    const char *cStr = [input UTF8String];
+    unsigned char digest[CC_MD5_DIGEST_LENGTH];
+    CC_MD5( cStr, strlen(cStr), digest ); // This is the md5 call
+    
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    
+    for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+        [output appendFormat:@"%02x", digest[i]];
+    
+    return  output;
+}
+@end
